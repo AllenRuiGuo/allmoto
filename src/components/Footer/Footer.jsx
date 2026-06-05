e8beaaa8229d58ae5edad8ab28c382b18171a5bb
Rui Guo <ruiguo.developer@outlook.com>
2026-06-05 16:29:08 +1000

    Add Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="site-footer">
+
+      <h2>Together, we can build the site of your dreams.</h2>
+
+      <button>Contact Us</button>
+
+      <p>© 2026 AllMoto</p>
+
+    </footer>
+  )
+}
+
+export default Footer
