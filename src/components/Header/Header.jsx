b2f26eefa5833fc5f3feb466220ec64b87a038c2
Rui Guo <ruiguo.developer@outlook.com>
2026-06-05 16:27:50 +1000

    Add Header.jsx and Header.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import './header.css'
+
+function Header() {
+  return (
+    <header className="site-header">
+      <div className="header-container">
+
+        <a href="/" className="brand">
+          <img src="/logofornavbar.png" alt="AllMoto" />
+          <span>AllMoto</span>
+        </a>
+
+        <nav className="site-nav">
+          <a href="/projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
