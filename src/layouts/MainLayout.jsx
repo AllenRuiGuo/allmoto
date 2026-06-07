762a3a24227e0fbce55d6fbf96baa08c58f00757
Rui Guo <ruiguo.developer@outlook.com>
2026-06-07 14:58:12 +1000

    Style the MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,18 @@
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import './MainLayout.css'
 
 function MainLayout({ children }) {
   return (
-    <>
+    <div className="layout">
       <Header />
-      <main>
+
+      <main className="main-content">
         {children}
       </main>
+
       <Footer />
-    </>
+    </div>
   )
 }
 
