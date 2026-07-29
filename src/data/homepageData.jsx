f68cf7f7c0c40f3f9a5202d518989b7acc668cb6
Rui Guo <ruiguo.developer@outlook.com>
2026-07-29 20:30:38 +1000

    Add banner data to homepageData.jsx

---
+++ src/data/homepageData.jsx
@@ -0,0 +1,8 @@
+const homepageData = {
+    banner: {
+        title: "Where ideas take shape",
+        subtitle: "Designing and building your online presence",
+        image: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Banner%20-%20Background.png?updatedAt=1746528852164",
+    },
+
+}
