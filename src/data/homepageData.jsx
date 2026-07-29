f7ac7345d6a5db1e7fbb7b168fe8a9101aa386e4
Rui Guo <ruiguo.developer@outlook.com>
2026-07-29 20:33:44 +1000

    Add about data to homepageData.jsx

--- src/data/homepageData.jsx
+++ src/data/homepageData.jsx
@@ -5,4 +5,12 @@ const homepageData = {
         image: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Banner%20-%20Background.png?updatedAt=1746528852164",
     },
 
+    about: {
+        titles: [
+            "About Us",
+            "Welcome to",
+            "Allmoto"
+        ],
+        introduction: "Empathy is the first step in design thinking because it is a skill that allows us to understand and share the same feelings that others feel. Through empathy, we are able to put ourselves in other people's shoes and connect with how they might be feeling about their problem, circumstance, or situation.",
+    },
 }
