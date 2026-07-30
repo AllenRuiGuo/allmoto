b2e0bd2c80865bc26f82e2eafde9b70430b5fbcd
Rui Guo <ruiguo.developer@outlook.com>
2026-07-30 18:14:40 +1000

    Add service data to homepageData.jsx

--- src/data/homepageData.jsx
+++ src/data/homepageData.jsx
@@ -13,4 +13,45 @@ const homepageData = {
         ],
         introduction: "Empathy is the first step in design thinking because it is a skill that allows us to understand and share the same feelings that others feel. Through empathy, we are able to put ourselves in other people's shoes and connect with how they might be feeling about their problem, circumstance, or situation.",
     },
+
+    service: {
+        title: "How it works",
+        description: "Starting an online small business from zero with limited funds and low risk!",
+        keyservices: [
+            {
+                id: 1,
+                title: "Design",
+                thumbnail: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Service%20Card%20-%20Design.png?updatedAt=1746528851683",
+                description: "UX design involves the design of the entire process of acquiring and integrating the product, including aspects of branding, design, usability and function.",
+                keypoints: [
+                    "User Research",
+                    "Interface Design",  
+                    "Prototyping & Testing",
+                ],
+            },
+            {
+                id: 2,
+                title: "Development",
+                thumbnail: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Service%20Card%20-%20Develop.png?updatedAt=1746528851646",
+                description: "We emphasize quality and efficiency, conducting thorough testing and iterative improvements to ensure that the product performs optimally and meets all specifications.",
+                keypoints: [
+                    "Responsive Design",
+                    "Tailored Development",
+                    "Ongoing Maintenance"
+                ]
+            },
+            {
+                id: 3,
+                title: "Marketing",
+                thumbnail: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Service%20Card%20-%20Marketing.png?updatedAt=1746528851706",     
+                description: "We conduct comprehensive market research and analysis to deeply understand your target audience and competitive landscape.",
+                keypoints: [
+                    "Brand Strategy",
+                    "Digital Advertising",
+                    "Content Marketing"
+                ]
+            }
+        ]
+    }
+
 }
