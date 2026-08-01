b6ddcc2d6154385ade1037ebcfa131b531c10729
Rui Guo <ruiguo.developer@outlook.com>
2026-08-01 15:03:25 +1000

    Add faq data to homepageData.jsx

--- src/data/homepageData.jsx
+++ src/data/homepageData.jsx
@@ -56,7 +56,35 @@ const homepageData = {
     whyus: {
         title: "Reliable, Efficient and Personalized only for you.",
         subtitle: "Allmoto", 
-        image: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Why%20Us.png?updatedAt=1746528852384",
-    }
+        imageurl: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Why%20Us.png?updatedAt=1746528852384",
+    },
+    faq: {
+        title: "Get the answers you are looking for",
+        subtitle: "HERE’S SOME FREQUENTLY ASKED QUESTIONS",
+        imageurl: "https://ik.imagekit.io/amdigitool/AMDigitool/Section%20Q%20&%20A.png?updatedAt=1746528852128",
+        qas: [
+            {
+                id: 1,
+                question: "What size businesses do you work with?",   
+                answer: "We work with businesses ranging from startups to medium-sized enterprises. Our flexible approach allows us to tailor our services to meet the unique needs and goals of each client, ensuring a successful partnership regardless of your company's stage of growth."  
+            },
+            {
+                id: 2,
+                question: "How much do your services cost?",   
+                answer: "Our service costs vary depending on the scope and complexity of each project. We provide customized quotes based on your specific needs, ensuring you get the best value for your investment. Please contact us for a detailed consultation and a tailored estimate."                
+            },
+            {
+                id: 3,
+                question: "Why should I hire a digital agency instead of doing it myself?",   
+                answer: "Hiring a digital agency provides you with access to a team of experts with specialized skills in areas like UX/UI design, web and app development, and digital strategy. This ensures that your project is executed professionally and efficiently, saving you time and avoiding costly mistakes. A digital agency brings a fresh perspective, industry insights, and the latest tools and technologies to deliver high-quality results that help your business grow and stand out in a competitive market."  
+            },
+            {
+                id: 4,
+                question: "How long will it take to finish my project?",   
+                answer: "The timeline for completing your project depends on its scope and complexity. During our initial consultation, we’ll work with you to define your project’s requirements and establish a clear timeline. We prioritize delivering high-quality work efficiently and will keep you informed throughout the process to ensure we meet your deadlines."  
+            }
+            ]       
+        }
+
 
 }
