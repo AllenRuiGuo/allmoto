b7210b79ee61753f599f5e08cd69189fd6b4515c
Rui Guo <ruiguo.developer@outlook.com>
2026-08-03 20:43:40 +1000

    Modify About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,29 @@
-function About() {
+import './About.css'
+
+function About({ about }) {
   return (
-    <section>
-      About
+    <section className="about">
+
+      <div className="about-container">
+
+        <div className="about-heading">
+
+          <h1>{about.titles[1]}</h1>
+
+          <h1>{about.titles[2]}</h1>
+
+        </div>
+
+        <div className="about-content">
+
+          {about.introductions.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+
+        </div>
+
+      </div>
+
     </section>
   )
 }
