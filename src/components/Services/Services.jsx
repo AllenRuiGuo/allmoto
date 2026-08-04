25a29a29a9f478fd0f60d241cce6f2dc9eccbaad
Rui Guo <ruiguo.developer@outlook.com>
2026-08-04 18:33:40 +1000

    Edit Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,31 @@
-function Services() {
+import './Services.css'
+import ServiceCard from '../ServiceCard/ServiceCard'
+
+function Services({ services }) {
   return (
-    <section>
-      Services
+    <section className="services">
+
+      <div className="services-container">
+
+        <h1>{services.title}</h1>
+
+        <p className="services-description">
+          {services.description}
+        </p>
+
+        <div className="services-grid">
+
+          {services.keyServices.map(service => (
+            <ServiceCard
+              key={service.id}
+              service={service}
+            />
+          ))}
+
+        </div>
+
+      </div>
+
     </section>
   )
 }
