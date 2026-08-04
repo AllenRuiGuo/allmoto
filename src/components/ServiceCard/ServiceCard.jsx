2280fecc8a5ba1b5879d4aaf284bcf30db342b31
Rui Guo <ruiguo.developer@outlook.com>
2026-08-04 18:35:01 +1000

    Edit ServiceCard.jsx

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -1,11 +1,40 @@
+import './ServiceCard.css'
+
 function ServiceCard({ service }) {
-  return (
-    <div className="service-card">
-      <img src={service.imageUrl} alt={service.title} />
-      <h3>{service.title}</h3>
-      <p>{service.description}</p>
-    </div>
-  )
+
+    return (
+        <div className="service-card">
+
+            <div className="service-card-content">
+
+                <img
+                    src={service.thumbnail}
+                    alt={service.title}
+                    className="service-card-image"
+                />
+
+
+                <h3>
+                    {service.title}
+                </h3>
+
+
+                <p>
+                    {service.description}
+                </p>
+
+
+                <a 
+                    href="/projects"
+                    className="service-card-button"
+                >
+                    Learn More
+                </a>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default ServiceCard
