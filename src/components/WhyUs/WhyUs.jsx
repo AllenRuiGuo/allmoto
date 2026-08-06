99f0a6bb3c83d305660e7080a322c5723db4349d
Rui Guo <ruiguo.developer@outlook.com>
2026-08-06 20:32:58 +1000

    Edit WhyUs.jsx

--- src/components/WhyUs/WhyUs.jsx
+++ src/components/WhyUs/WhyUs.jsx
@@ -1,9 +1,28 @@
-function WhyUs() {
-  return (
-    <section>
-      Why Us
-    </section>
-  )
+import './WhyUs.css'
+
+function WhyUs({ whyUs }) {
+    return (
+        <section
+            className="why-us"
+            style={{ backgroundImage: `url(${whyUs.imageUrl})` }}
+        >
+            <div className="why-us-overlay">
+
+                <div className="why-us-container">
+
+                    <h5 className="why-us-subtitle">
+                        {whyUs.subtitle}
+                    </h5>
+
+                    <h1 className="why-us-title">
+                        {whyUs.title}
+                    </h1>
+
+                </div>
+
+            </div>
+        </section>
+    )
 }
 
 export default WhyUs
