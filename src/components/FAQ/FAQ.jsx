b6ef5dd87689f048ec6c6fbad2e42c332e8cfc03
Rui Guo <ruiguo.developer@outlook.com>
2026-08-07 17:55:50 +1000

    Add FAQ.jsx

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,9 +1,50 @@
-function FAQ() {
-  return (
-    <section>
-      FAQ
-    </section>
-  )
+import './FAQ.css'
+import AccordionItem from '../AccordionItem/AccordionItem'
+
+function FAQ({ faq }) {
+
+    return (
+        <section className="faq">
+
+            <div className="faq-container">
+
+                <div className="faq-image">
+
+                    <img
+                        src={faq.imageUrl}
+                        alt="Frequently Asked Questions"
+                    />
+
+                </div>
+
+                <div className="faq-content">
+
+                    <h6 className="faq-subtitle">
+                        {faq.subtitle}
+                    </h6>
+
+                    <h2 className="faq-title">
+                        {faq.title}
+                    </h2>
+
+                    <div className="faq-list">
+
+                        {faq.qas.map(item => (
+                            <AccordionItem
+                                key={item.id}
+                                question={item.question}
+                                answer={item.answer}
+                            />
+                        ))}
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
 }
 
 export default FAQ
