5b2f5be8f2dcd426b3b53d99e1ca12994e9889a5
Rui Guo <ruiguo.developer@outlook.com>
2026-08-13 17:53:44 +1000

    Add projectsData.jsx

---
+++ src/data/projectsData.jsx
@@ -0,0 +1,156 @@
+const projectsData = [
+    {
+        id: "MicroBank",
+        title: "MicroBank",
+        subtitle: "A digital bank which is for young adults.",
+        category: "Design",
+
+        thumbnail:
+            "https://ik.imagekit.io/amdigitool/AMDigitool/Microbank%20-%20thumb.jpg?updatedAt=1747044339309",
+
+        bannerImage:
+            "https://ik.imagekit.io/amdigitool/AMDigitool/Microbank%20-%200.jpg?updatedAt=1747043399259",
+
+        content: `
+            <h1>How we help our clients digitalize their products to appeal to younger adults</h1>
+
+            <h3>1. Understanding the target audience</h3>
+
+            <p>
+                The first step is to understand the preferences, behaviors, and needs of younger adults.
+            </p>
+
+            <ul>
+                <li>
+                    Market Research: Conduct surveys, focus groups, and online research to gather insights into their digital habits, favorite platforms, and purchasing behaviors.
+                </li>
+                <li>
+                    Persona Development: Create detailed personas representing different segments of the younger adult market to guide your strategy.
+                </li>
+            </ul>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Microbank%20-%201.jpg?updatedAt=1746961796439"
+                    alt="MicroBank branding"
+                />
+            </div>
+
+            <h3>2. Develop a Mobile App</h3>
+
+            <p>
+                A mobile app can enhance user experience and loyalty.
+            </p>
+
+            <ul>
+                <li>
+                    App Features: Include features like personalized recommendations, push notifications for deals, and a seamless checkout process.
+                </li>
+                <li>
+                    User Experience: Focus on a smooth, intuitive user experience. Test the app extensively to eliminate bugs and ensure high performance.
+                </li>
+                <li>
+                    Rewards and Loyalty Programs: Implement a rewards system to encourage regular use and purchases.
+                </li>
+            </ul>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Microbank%20-%202.jpg?updatedAt=1746961796383"
+                    alt="MicroBank user flow"
+                />
+            </div>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Microbank%20-%203.jpg?updatedAt=1746961796404"
+                    alt="MicroBank screens"
+                />
+            </div>
+
+            <h3>3. Testing</h3>
+
+            <p>
+                Five younger adults used the prototype of the App and gave some feedback.
+            </p>
+
+            <ul>
+                <li>
+                    "The navigation of the app is incredibly intuitive. I am able to find all the key features, like viewing my account balance and transaction history, without any confusion. The design is clean and user-friendly."
+                </li>
+
+                <li>
+                    "I love how easy it is to transfer money using this app. The process is straightforward and fast. It's great that I can also save frequent payees for quicker transactions in the future."
+                </li>
+            </ul>
+        `
+    },
+
+    {
+        id: "ProtectionBracelet",
+        title: "Protection Bracelet",
+        subtitle:
+            "This is the first step if you want to attract prosperity and luck for the time ahead.",
+
+        category: "Development",
+
+        thumbnail:
+            "https://ik.imagekit.io/amdigitool/AMDigitool/Bracelet%20-%20thumb.jpg?updatedAt=1747044091877",
+
+        bannerImage:
+            "https://ik.imagekit.io/amdigitool/AMDigitool/Bracelet%20-%200.jpg?updatedAt=1747043399404",
+
+        content: `
+            <h1>The Challenge</h1>
+
+            <p>
+                According to the founder of Bodhi Balance, market research showed a 70 percent increase in searches for energy-enhancing and healing bracelets on popular e-commerce platforms.
+            </p>
+
+            <p>
+                Seeing this trend, Bodhi Balance decided to create a digital platform offering a unique selection of bracelets designed to promote well-being and personal expression.
+            </p>
+
+            <h1>What we did from Zero</h1>
+
+            <p>
+                We developed a dedicated digital platform that not only offers a unique selection of handcrafted, wellness-focused bracelets but also provides valuable resources to enhance personal energy and well-being.
+            </p>
+
+            <p>
+                Our online store is designed for an easy and enjoyable shopping experience.
+            </p>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Bracelet%20-%201.jpg?updatedAt=1746961619716"
+                    alt="Bodhi Balance Homepage"
+                />
+            </div>
+
+            <p>
+                The purchase and payment process is streamlined and secure, ensuring that clients can complete their transactions with ease and confidence.
+            </p>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Bracelet%20-%202.jpg?updatedAt=1746961619107"
+                    alt="Bodhi Balance order page"
+                />
+            </div>
+
+            <p>
+                To further support our customers, we introduced online workshops on mindfulness, yoga, and holistic health practices.
+            </p>
+
+            <div class="project-content-image">
+                <img
+                    src="https://ik.imagekit.io/amdigitool/AMDigitool/Bracelet%20-%203.jpg?updatedAt=1746961619751"
+                    alt="Bodhi Balance"
+                />
+            </div>
+        `
+    }
+]
+
+export default projectsData
