4fb6f957726f518db40985c49d9228b01adac2ae
Rui Guo <ruiguo.developer@outlook.com>
2026-08-19 18:44:44 +1000

    Fix the Home page import issue and Logo size in the header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
 import Projects from './pages/Projects/Projects'
 import ProjectDetails from './pages/ProjectDetails/ProjectDetails'
 
