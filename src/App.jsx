3addda25d0cb9c834c090d171e6b73782313ec59
Rui Guo <ruiguo.developer@outlook.com>
2026-08-19 18:10:21 +1000

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import MainLayout from './layouts/MainLayout'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+import Home from './pages/Home/Home'
+import Projects from './pages/Projects/Projects'
+import ProjectDetails from './pages/ProjectDetails/ProjectDetails'
 
 function App() {
-  return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
-  )
+    return (
+        <BrowserRouter>
+
+            <Header />
+
+            <Routes>
+
+                <Route path="/" element={<Home />} />
+
+                <Route
+                    path="/projects"
+                    element={<Projects />}
+                />
+
+                <Route
+                    path="/projects/:projectId"
+                    element={<ProjectDetails />}
+                />
+
+            </Routes>
+
+            <Footer />
+
+        </BrowserRouter>
+    )
 }
 
 export default App
